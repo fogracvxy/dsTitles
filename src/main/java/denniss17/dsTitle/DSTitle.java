312cf53b4f2dfc2030ec687addb190f844616d3e
MSpudic <marn1002000@gmail.com>
2019-07-24 14:03:30 +0200

    Update DSTitle.java
    Wrong Push fix

--- src/main/java/denniss17/dsTitle/DSTitle.java
+++ src/main/java/denniss17/dsTitle/DSTitle.java
@@ -45,7 +45,7 @@ public class DSTitle extends JavaPlugin{
 		        placeHolders = true;
 		    }
 		}
-		if(Bukkit.getPluginManager().isPluginEnabled("DeluxeChatPaid"))
+		if(Bukkit.getPluginManager().isPluginEnabled("DeluxeChat"))
 		{
 			if (denniss17.dsTitle.Placeholders.DeluxeChatHook.RegisterDeluxeChatHooks(title)) {
 		        getLogger().info("dsTitle was successfully registered with DeluxeChat!");
